1a09c7ea9dd447d65d7044a68d14a51edc49a418
AG <ayon.gupta@wwindia.com>
2019-07-24 17:43:10 +0530

    Added comment in app delegate

--- TruthOrDare/AppDelegate.swift
+++ TruthOrDare/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //Comment for did finish launching
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         Fabric.with([Crashlytics.self])
